198e832c782359e7bc8437570b628927d913b7bb
Maurine Karanja <113859496+MaurineKaranja@users.noreply.github.com>
2024-03-24 16:52:28 +0000

    stages

--- hangman_game.py
+++ hangman_game.py
@@ -1,36 +1,29 @@
 import random
-
+from hangman_stages import hangman_stages
 word_list = ["apples", "bananas", "oranges", "grapes", "pears", "strawberries"]
-chosen_word = random.choice(word_list)
-
-display = ['_' for _ in chosen_word]
-end_of_game = False
-guessed_letters = set()
-
-def update_display(letter):
-    global display
-    for i, char in enumerate(chosen_word):
-        if char == letter:
-            display[i] = letter
-
-def print_display():
-    print(' '.join(display))
-
-def game_over():
-    global end_of_game
-    if '_' not in display:
-        end_of_game = True
-        print("You win")
-
+chosen_word=random.choice(word_list)
+print(chosen_word)
+lives=6
+display=[]
+for i in range(len(chosen_word)):
+  display+="_"
+print(display)
+end_of_game=False
 while not end_of_game:
-    guess = input("Guess a letter: ").lower()
-    if len(guess) != 1 or not guess.isalpha():
-        print("Invalid input. Please enter a single letter.")
-    elif guess in guessed_letters:
-        print("You've already guessed that letter.")
-    else:
-        guessed_letters.add(guess)
-        update_display(guess)
-        print_display()
-        game_over()
+  guess=input("Guess a letter: ").lower()
+  for position in range(len(chosen_word)):
+    letter=chosen_word[position]
+    if letter==guess:
+      display[position]=letter
+  if guess not in chosen_word:
+    lives-=1
+    if lives==0:
+      end_of_game=True
+      print("You lose!!")
+  print(display)
+  if "_" not in display:
+    end_of_game=True
+    print("You win!!")
+  print(hangman_stages[lives])
+    
     
